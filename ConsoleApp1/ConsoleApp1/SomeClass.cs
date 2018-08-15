a185b57ad985620e2350895d8db8758d510d8238
Артем <tymchenkoartem@gmail.com>
2018-08-16 00:05:48 +0300

    FirstCommit 1
    project1 2
    
    signature refactoring 3
    
    functionality 4
    
    summaries 5

---
+++ ConsoleApp1/ConsoleApp1/SomeClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// My super dupper class.
+    /// </summary>
+    public class SomeClass
+    {
+        public void OneMethod1(int x, double y)
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            var result = x + y + massObjects.Length;
+            Console.WriteLine(result);
+        }
+    }
+}
